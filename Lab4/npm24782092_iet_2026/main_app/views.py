d2a13afc3861e0fe2b4421937ae2bd0e74d093d8
rendi <rendisaputra6788@gmail.com>
2026-04-08 14:14:44 +0700

    Merge branch main

---
+++ Lab4/npm24782092_iet_2026/main_app/views.py
@@ -0,0 +1,45 @@
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView, DetailView
+from django.urls import reverse_lazy
+from .models import Report
+
+# READ
+class ReportListView(ListView):
+    model = Report
+    template_name = 'main_app/home.html'
+    context_object_name = 'reports'
+
+# CREATE
+class ReportCreateView(CreateView):
+    model = Report
+    fields = ['title', 'category', 'description', 'location']
+    template_name = 'main_app/add_report.html'
+    success_url = reverse_lazy('home')
+
+# UPDATE
+class ReportUpdateView(UpdateView):
+    model = Report
+    fields = ['title', 'category', 'description', 'location']
+    template_name = 'main_app/add_report.html'
+    success_url = reverse_lazy('home')
+
+# DELETE
+class ReportDeleteView(DeleteView):
+    model = Report
+    template_name = 'main_app/delete_confirm.html'
+    success_url = reverse_lazy('home')
+
+# DETAIL
+class ReportDetailView(DetailView):
+    model = Report
+    template_name = 'main_app/detail.html'
+
+from django.views import View
+from django.shortcuts import get_object_or_404, redirect
+
+class ReportUpdateStatusView(View):
+    def post(self, request, pk):
+        report = get_object_or_404(Report, pk=pk)
+        new_status = request.POST.get('status')
+        report.status = new_status
+        report.save()
+        return redirect('home')
